54a6b159702c0bedfd0eef727757ed826695f107
Yuriy Khomyk <khomykyu@gmail.com>
2018-08-30 22:48:25 +0300

    Update factories

--- Factory/chicagopizzaingredientfactory.cpp
+++ Factory/chicagopizzaingredientfactory.cpp
@@ -3,31 +3,31 @@
 
 std::unique_ptr<Dough> ChicagoPizzaIngredientFactory::createDought() const
 {
-    return std::make_unique<ThickCrustDough>("");
+    return std::make_unique<ThickCrustDough>();
 }
 
 std::unique_ptr<Sauce> ChicagoPizzaIngredientFactory::createSauce() const
 {
-    return std::make_unique<PlumTomatoSauce>("");
+    return std::make_unique<PlumTomatoSauce>();
 }
 
 std::unique_ptr<Cheese> ChicagoPizzaIngredientFactory::createCheese() const
 {
-    return std::make_unique<MozzarellaCheese>("");
+    return std::make_unique<MozzarellaCheese>();
 }
 
 std::unique_ptr<Pepperoni> ChicagoPizzaIngredientFactory::createPepperoni() const
 {
-    return std::make_unique<SlicedPepperoni>("");
+    return std::make_unique<SlicedPepperoni>();
 }
 
 std::unique_ptr<Clams> ChicagoPizzaIngredientFactory::createClam() const
 {
-    return std::make_unique<FrozenClams>("");
+    return std::make_unique<FrozenClams>();
 }
 
 std::vector<std::unique_ptr<Veggies> > ChicagoPizzaIngredientFactory::createVeggies() const
 {
-    return {std::make_unique<BlackOlives>(""), std::make_unique<Spinach>(""),
-            std::make_unique<Eggplant>("")};
+    return {std::make_unique<BlackOlives>(), std::make_unique<Spinach>(),
+            std::make_unique<Eggplant>()};
 }
--- Factory/nypizzaingredientfactory.cpp
+++ Factory/nypizzaingredientfactory.cpp
@@ -3,31 +3,31 @@
 
 std::unique_ptr<Dough> NYPizzaIngredientFactory::createDought() const
 {
-    return std::make_unique<ThinCrustDough>("");
+    return std::make_unique<ThinCrustDough>();
 }
 
 std::unique_ptr<Sauce> NYPizzaIngredientFactory::createSauce() const
 {
-    return std::make_unique<MarinaraSauce>("");
+    return std::make_unique<MarinaraSauce>();
 }
 
 std::unique_ptr<Cheese> NYPizzaIngredientFactory::createCheese() const
 {
-    return std::make_unique<ReggianoCheese>("");
+    return std::make_unique<ReggianoCheese>();
 }
 
 std::unique_ptr<Pepperoni> NYPizzaIngredientFactory::createPepperoni() const
 {
-    return std::make_unique<SlicedPepperoni>("");
+    return std::make_unique<SlicedPepperoni>();
 }
 
 std::unique_ptr<Clams> NYPizzaIngredientFactory::createClam() const
 {
-    return std::make_unique<FreshClams>("");
+    return std::make_unique<FreshClams>();
 }
 
 std::vector<std::unique_ptr<Veggies> > NYPizzaIngredientFactory::createVeggies() const
 {
-    return {std::make_unique<Garlic>(""), std::make_unique<Onion>(""),
-            std::make_unique<Mushroom>(""), std::make_unique<RedPepper>("")};
+    return {std::make_unique<Garlic>(), std::make_unique<Onion>(),
+            std::make_unique<Mushroom>(), std::make_unique<RedPepper>()};
 }
