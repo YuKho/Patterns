1572a4b164045a2ddb6d4db8848fbc2ccd2b135c
YuKho <khomykyu@gmail.com>
2018-09-02 00:44:20 +0300

    Update chicago store

--- Factory/chicagopizzastore.h
+++ Factory/chicagopizzastore.h
@@ -2,32 +2,39 @@
 #define CHICAGOPIZZASTORE_H
 
 #include "pizzastore.h"
-#include "chicagopizzas.h"
+#include "pizzas.h"
+#include "chicagopizzaingredientfactory.h"
 #include <memory>
 
 class ChicagoPizzaStore : public PizzaStore
 {
+public:
+    ChicagoPizzaStore() : _factory{std::make_shared<ChicagoPizzaIngredientFactory>()} {}
+
 private:
     std::unique_ptr<Pizza> createPizza(const std::string &name) const override
     {
         switch (getPizzaType(name))
         {
         case PizzaType::Cheese:
-            return std::make_unique<ChicagoStyleCheesePizza>();
+            return std::make_unique<CheesePizza>("Chicago Style Cheese Pizza", _factory);
 
         case PizzaType::Pepperoni:
-            return std::make_unique<ChicagoStylePepperoniPizza>();
+            return std::make_unique<PepperoniPizza>("Chicago Style Pepperoni Pizza", _factory);
 
         case PizzaType::Clam:
-            return std::make_unique<ChicagoStyleClamPizza>();
+            return std::make_unique<ClamPizza>("Chicago Style Clam Pizza", _factory);
 
         case PizzaType::Veggie:
-            return std::make_unique<ChicagoStyleVeggiePizza>();
+            return std::make_unique<VeggiePizza>("Chicago Style Veggie Pizza", _factory);
 
         default:
             return nullptr;
         }
     }
+
+private:
+    std::shared_ptr<PizzaIngredientFactory> _factory;
 };
 
 #endif // CHICAGOPIZZASTORE_H
